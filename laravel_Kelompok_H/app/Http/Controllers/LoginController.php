318b427b8e7ffb1c22b1554b9bd00321cc2548aa
Edmond3003 <edmondsoren@gmail.com>
2022-12-14 12:55:04 +0700

    first commit

---
+++ laravel_Kelompok_H/app/Http/Controllers/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /**
+     * login
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login Failed!',
+            ]);
+        }
+        $token = $user->createToken('Authentication Token')->accessToken;
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Success!',
+            'data'    => $user,
+            'token'   => $token
+        ]);
+    }
+
+    /**
+     * logout
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function logout(Request $request)
+    {
+        $removeToken = $request->user()->tokens()->delete();
+
+        if($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Success!',
+            ]);
+        }
+    }
+}
